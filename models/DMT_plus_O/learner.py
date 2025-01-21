48fb7c3a35b08f7b5fd2cd0520a656b6d38ca95d
lxtx123 <1278311622@qq.com>
2025-01-21 15:49:21 +0800

    Add files via upload

---
+++ models/DMT_plus_O/learner.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from conv4d import CenterPivotConv4d as Conv4d
+from icecream import ic
+
+class HPNLearner(nn.Module):
+    def __init__(self, inch):
+        super(HPNLearner, self).__init__()
+
+        def make_building_block(in_channel, out_channels, kernel_sizes, spt_strides, group=4):
+            assert len(out_channels) == len(kernel_sizes) == len(spt_strides)
+
+            building_block_layers = []
+            for idx, (outch, ksz, stride) in enumerate(zip(out_channels, kernel_sizes, spt_strides)):
+                inch = in_channel if idx == 0 else out_channels[idx - 1]
+                ksz4d = (ksz,) * 4
+                str4d = (1, 1) + (stride,) * 2
+                pad4d = (ksz // 2,) * 4
+                building_block_layers.append(Conv4d(inch, outch, ksz4d, str4d, pad4d))
+                building_block_layers.append(nn.GroupNorm(group, outch))
+                building_block_layers.append(nn.ReLU(inplace=True))
+
+            return nn.Sequential(*building_block_layers)
+        outch1, outch2, outch3 = 64, 128, 256
+
+        # Squeezing building blocks
+        self.enc_layer = make_building_block(inch, [outch1, outch2, outch3], [3, 3, 3], [1, 1, 1])
+
+    def forward(self, hypercorr_pyramid, w_mask):
+        hypercorr_sqz = self.enc_layer(hypercorr_pyramid)
+        bs = hypercorr_sqz.shape[-1]
+        w_mask = torch.sigmoid(w_mask)
+        w_mask = torch.matmul(w_mask.squeeze().view(-1, 1), w_mask.squeeze().view(1, -1))
+        hypercorr_sqz = hypercorr_sqz * w_mask.view(bs, 1, 1, 1, bs)
+        hypercorr_encoded = hypercorr_sqz.mean(dim=-1)
+        return hypercorr_encoded
